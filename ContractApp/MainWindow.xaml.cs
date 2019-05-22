d945b13b68fe8ee43a57187d6513e7564a0e76a6
YerikMeg <asanali2001@mail.ru>
2019-05-23 00:25:26 +0600

    HomeWork

---
+++ ContractApp/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+namespace ContractApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void PrintButtonClick(object sender, RoutedEventArgs e)
+        {
+            PrintDialog printDialog = new PrintDialog();
+            IDocumentPaginatorSource source = contractDocument;
+
+            if (printDialog.ShowDialog() == true)
+            {
+                printDialog.PrintDocument(source.DocumentPaginator, "Contract document");
+            }
+        }
+    }
+}
